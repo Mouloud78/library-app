22557dad08abaeab7cd64f19430fc06b85a8a703
mouloud78 <mouloudouldali78@gmail.com>
2024-01-14 14:26:40 -0500

    add navbar

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+
+function App() {
+  return (
+    <Navbar />
+  );
+}
+
+export default App;
